d9a45437b7d5cbf64b89da472699d61aafbfe89c
Yunyun SHEN <ys270224@is156481.intra.cea.fr>
2022-10-18 17:11:10 +0200

    Change the position to lower right

--- Pilot_coding/Plot_feedback.m
+++ Pilot_coding/Plot_feedback.m
@@ -5,6 +5,8 @@ screenXpixels = P.ScreenWidth;
 screenYpixels = P.ScreenHeight;
 x_step = screenXpixels/50;
 y_step = screenYpixels/10;
+
+
 lwidth = P.xcenter-x_step*1;
 rwidth = P.xcenter+x_step*1;
 
@@ -13,7 +15,8 @@ numb_rect = length(estimation_relative);
 
 % when very good perf, make a green bar and display BRAVO!
 if sum(abs(estimation_relative) < 0.1)/numb_rect == 1
-    DrawFormattedText(w, 'BRAVO!!', 'center', 'center', P.Color.black);
+    DrawFormattedText(w, 'BRAVO!! ',P.xcenter+x_step*9,P.ycenter+y_step*2, P.Color.black);
+    DrawFormattedText(w, '+', 'center', 'center', P.Color.black);
 %     Screen('FillRect', w, P.Color.green, [P.xcenter-x_step*6, P.ycenter-y_step*4,P.xcenter+x_step*6, P.ycenter+y_step*4]);
     Screen('Flip', w);
     
@@ -23,11 +26,11 @@ else
     xy_rect = NaN(1,7);
     
     if numb_rect == 1
-        xl_rect = [lwidth nan nan nan nan nan nan];
-        xy_rect = [rwidth nan nan nan nan nan nan];
+        xl_rect = [lwidth+x_step*12 nan nan nan nan nan nan];
+        xy_rect = [rwidth+x_step*12 nan nan nan nan nan nan];
     elseif numb_rect == 2
-        xl_rect=[lwidth-x_step*2 lwidth+x_step*2 nan nan nan nan nan];
-        xy_rect=[rwidth-x_step*2 rwidth+x_step*2 nan nan nan nan nan];
+        xl_rect=[lwidth+x_step*9 lwidth+x_step*14 nan nan nan nan nan];
+        xy_rect=[rwidth+x_step*9 rwidth+x_step*14 nan nan nan nan nan];
 %     elseif numb_rect == 3
 %         x_rect=[lwidth+x_step*2.5 lwidth lwidth-x_step*2.5 nan nan nan nan];
 %         X_rect=[rwidth+x_step*2.5 rwidth rwidth-x_step*2.5 nan nan nan nan];
@@ -45,16 +48,20 @@ else
     
     for i = 1:numb_rect
         if estimation_relative(i) < 0
-            y_rect(i) = P.ycenter + y_step*estimation_relative(i)*-1;
-            Y_rect(i) = P.ycenter;
+%             y_rect(i) = P.ycenter + y_step*estimation_relative(i)*-1;
+%             Y_rect(i) = P.ycenter;
+            y_rect(i) = P.ycenter+y_step*2+ y_step*estimation_relative(i)*-1;
+            Y_rect(i) = P.ycenter+y_step*2;
         else
-            y_rect(i) = P.ycenter;
-            Y_rect(i) = P.ycenter + y_step*estimation_relative(i)*-1;
+%             y_rect(i) = P.ycenter;
+%             Y_rect(i) = P.ycenter + y_step*estimation_relative(i)*-1;
+            y_rect(i) = P.ycenter+y_step*2;
+            Y_rect(i) = P.ycenter+y_step*2 + y_step*estimation_relative(i)*-1;
         end
     end
     allRects =  [xl_rect; y_rect; xy_rect; Y_rect];
-     
     allColors = NaN(3,7);
+
     for k = 1:numb_rect
         if abs(estimation_relative(k)) <= 0.1
             allColors(:,k) = P.Color.black;
@@ -64,7 +71,9 @@ else
             allColors(:,k) = P.Color.black;
         end        
         Screen('FillRect', w, allColors, allRects);
-        Screen('FillRect', w, P.Color.black, [P.xcenter-x_step*10, P.ycenter-1,P.xcenter+x_step*10, P.ycenter+1]);
+        Screen('FillRect', w, P.Color.black, [P.xcenter+x_step*2, P.ycenter-1+y_step*2,P.xcenter+x_step*22, P.ycenter+1+y_step*2]);
+        
+        DrawFormattedText(w,'+','center','center',P.Color.black);
         Screen('Flip', w);
     end
 end
